158b61604547cb688c7af723f020958093022a99
yzcode <1540032876@qq.com>
2022-03-16 22:11:48 +0800

    feat: add echart and echard-wordcloud

--- src/components/home/article-card/ArticleCard.vue
+++ src/components/home/article-card/ArticleCard.vue
@@ -7,6 +7,7 @@
     <template #second-title> 笔记词云统计 </template>
     <template #content>
       <div class="content">
+        <div class="article-charts" ref="chartRef" />
         <img
           class="article-background"
           src="../../../assets/img/home_article.svg"
@@ -17,17 +18,85 @@
 </template>
 
 <script setup lang="ts">
+import { ref, onMounted, reactive } from "vue";
+import * as echarts from "echarts";
+import "echarts-wordcloud";
 import { ChatLineSquare as IconChatLineSquare } from "@element-plus/icons-vue";
+
+interface ArticleType {
+  value: number;
+  name: string;
+}
+
+const chartRef = ref(null);
+let articleTypeList: ArticleType[] = reactive([]);
+
+const getArticleTypeList = () => {
+  articleTypeList = [
+    { value: Math.floor(Math.random() * 256), name: "Vue" },
+    { value: Math.floor(Math.random() * 256), name: "React" },
+    { value: Math.floor(Math.random() * 256), name: "Html" },
+    { value: Math.floor(Math.random() * 256), name: "Css" },
+    { value: Math.floor(Math.random() * 256), name: "Design" },
+    { value: Math.floor(Math.random() * 256), name: "随笔" },
+    { value: Math.floor(Math.random() * 256), name: "ES" },
+    { value: Math.floor(Math.random() * 256), name: "牛逼" },
+    { value: Math.floor(Math.random() * 256), name: "哇哦" },
+    { value: Math.floor(Math.random() * 256), name: "好棒哦" },
+    { value: Math.floor(Math.random() * 256), name: "哥哥厉害" },
+  ];
+};
+
+const initChartsOption = () => {
+  if (!chartRef.value) return;
+  let chart = echarts.init(chartRef.value);
+  chart.setOption({
+    series: [
+      {
+        type: "wordCloud",
+        shape: "diamond",
+        width: "100%",
+        height: "100%",
+        sizeRange: [18, 30],
+        rotationRange: [0, 0],
+        rotationStep: 45,
+        gridSize: 20,
+        drawOutOfBound: false,
+        textStyle: {
+          fontFamily: "Franklin Gothic Medium",
+          fontWeight: "bold",
+          color: () => {
+            return `rgb(
+              ${Math.floor(Math.random() * 256)},
+              ${Math.floor(Math.random() * 256)},
+              ${Math.floor(Math.random() * 256)}
+            )`;
+          },
+        },
+        data: articleTypeList,
+      },
+    ],
+  });
+};
+
+onMounted(() => {
+  getArticleTypeList();
+  initChartsOption();
+});
 </script>
 
 <style scoped lang="scss">
 .title-text {
   margin-left: 4px;
 }
+.article-charts {
+  width: 400px;
+  height: 120px;
+}
 .article-background {
   position: absolute;
   right: -40px;
-  bottom: -170px;
+  bottom: -50px;
   height: 200px;
   opacity: 0.5;
 }
