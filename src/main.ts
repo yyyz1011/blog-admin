36e7cde51a95c90cbb0eecbf0674dcf25dff81cf
yzcode <1540032876@qq.com>
2022-03-20 17:04:30 +0800

    feat: 增加el-reset文件

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./routers";
 import "@/assets/css/common.scss";
 import "element-plus/dist/index.css";
+import "@/assets/css/el-reset.scss";
 
 const app = createApp(App);
 app.use(router);
