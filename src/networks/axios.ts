0670030bd2d94a7b5acf5ab0cddd3c18675536ae
yzcode <1540032876@qq.com>
2022-03-28 23:25:56 +0800

    feat: add axios part

---
+++ src/networks/axios.ts
@@ -0,0 +1,110 @@
+import axios, {AxiosRequestConfig, Method} from "axios";
+import {ElMessage} from "element-plus";
+
+const baseUrl = "http://127.0.0.1:3000/api";
+
+// 定义接口
+interface PendingType {
+  url?: string;
+  method?: Method;
+  params: any;
+  data: any;
+  cancel: any;
+}
+
+// 取消重复请求
+const pending: Array<PendingType> = [];
+const CancelToken = axios.CancelToken;
+
+// 移除重复请求
+const removePending = (config: AxiosRequestConfig) => {
+  for (const key in pending) {
+    const item: number = +key;
+    if (pending.hasOwnProperty(key)) {
+      const list: PendingType = pending[key];
+      // 当前请求在数组中存在时执行函数体
+      if (
+        list.url === config.url &&
+        list.method === config.method &&
+        JSON.stringify(list.params) === JSON.stringify(config.params) &&
+        JSON.stringify(list.data) === JSON.stringify(config.data)
+      ) {
+        list.cancel("操作太频繁，请稍后再试");
+        pending.splice(item, 1);
+      }
+    }
+  }
+};
+
+// 实例化请求
+const instance = axios.create({
+  headers: {
+    "Content-Type": "application/json;charset=UTF-8",
+    "Access-Control-Allow-Origin-Type": "*"
+  },
+  baseURL: baseUrl,
+  timeout: 5 * 1000,
+  withCredentials: false,
+});
+
+// 请求拦截器
+instance.interceptors.request.use(
+  config => {
+    removePending(config);
+    config.cancelToken = new CancelToken(c => {
+      pending.push({url: config.url, method: config.method, params: config.params, data: config.data, cancel: c});
+    });
+    // 登录流程控制中，根据本地是否存在token判断用户的登录情况
+    // 但是即使token存在，也有可能token是过期的，所以在每次的请求头中携带token
+    // 后台根据携带的token判断用户的登录情况，并返回给我们对应的状态码
+    // 而后我们可以在响应拦截器中，根据状态码进行一些统一的操作。
+    // TODO 对token进行操作
+
+    return config;
+  },
+  err => {
+    ElMessage.error(err.data.error.message);
+    return Promise.reject(err.data.error.message);
+  }
+);
+
+// 相应拦截器
+instance.interceptors.response.use(
+  config => {
+    removePending(config.config);
+    if (config.status === 200) {
+      return Promise.resolve(config);
+    } else {
+      return Promise.reject(config);
+    }
+  }, err => {
+    const {response} = err;
+    if (response) {
+      const config = err.config;
+      const [RETRY_COUNT, RETRY_DELAY] = [3, 1000];
+
+      if (config && RETRY_COUNT) {
+        // 设置用于跟踪重试计数的变量
+        config.__retryCount = config.__retryCount || 0;
+        // 检查是否已经把重试的总数用完
+        if (config.__retryCount >= RETRY_COUNT) {
+          return Promise.reject(response || {message: err.message});
+        }
+        // 增加重试计数
+        config.__retryCount++;
+        // 创造新的Promise来处理指数后退
+        const backoff = new Promise<void>((resolve) => {
+          setTimeout(() => {
+            resolve();
+          }, RETRY_DELAY || 1);
+        });
+        // instance重试请求的Promise
+        return backoff.then(() => {
+          return instance(config);
+        });
+      }
+      return Promise.reject(response);
+    }
+  });
+
+export default instance;
